d10bdc29a933618c3e8d03af3c7e83c1ba91d963
u25176502-commits <u25176502@tuks.co.za>
2026-05-23 13:46:42 +0200

    Added DB Connection
    I made the database in myphpadmin and added the database credentials to the file

---
+++ Config/db.php
@@ -0,0 +1,24 @@
+<?php
+     $host = 'wheatley.cs.up.ac.za';
+     $db   = 'u25176502_tripistry';
+     $user = 'u25176502'; 
+     $pass = 'M5RNWUCZ4Z6TTBWXRPAY5Y7RB2CZLDUD'; // Set your MariaDB password here
+     $charset = 'utf8mb4';
+
+     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+     $options = [
+     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+     PDO::ATTR_EMULATE_PREPARES   => false, 
+     ];
+
+     try {
+          $pdo = new PDO($dsn, $user, $pass, $options);
+          $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          echo "Connected successfully";
+          error_log("Connected successfully");
+     } catch (\PDOException $e) {
+          // throw new \PDOException($e->getMessage(), (int)$e->getCode());
+          echo $e;
+     }
+?>
