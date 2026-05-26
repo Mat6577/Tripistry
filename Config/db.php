d8d1a168f5fb21b45f7e0eeccfd7638dee5455ec
u25176502-commits <u25176502@tuks.co.za>
2026-05-26 10:56:02 +0200

    added environment variables

--- Config/db.php
+++ Config/db.php
@@ -1,15 +1,24 @@
 <?php
-     $host = 'wheatley.cs.up.ac.za';
-     $db   = 'u25176502_tripistry';
-     $user = 'u25176502'; 
-     $pass = 'M5RNWUCZ4Z6TTBWXRPAY5Y7RB2CZLDUD'; // Set your MariaDB password here
+     require dirname(__DIR__) . '/vendor/autoload.php';
+     
+     use Dotenv\Dotenv;
+
+     // Load environment variables from .env file
+     $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+     $dotenv->load();
+
+     // Retrieve environment variables
+     $host = $_ENV['DB_HOST'];
+     $db = $_ENV['DB_DATABASE'];
+     $user = $_ENV['DB_USER'];
+     $pass = $_ENV['DB_PASS'];
      $charset = 'utf8mb4';
 
      $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
      $options = [
-     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-     PDO::ATTR_EMULATE_PREPARES   => false, 
+          PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+          PDO::ATTR_EMULATE_PREPARES   => false, 
      ];
 
      try {
